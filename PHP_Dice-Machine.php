13a41fb76f60a8677183ca7466c79d23e5fce6f0
Niguma Web Development <44173219+Niguma@users.noreply.github.com>
2019-02-27 14:21:47 +0100

    Add files via upload

---
+++ PHP_Dice-Machine.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Behind the Curtain...where trained half-crazy monkeys <br> working hard on your request</h1>
+
+<?php
+echo "Hello World!";
+?>
+
+</body>
+</html> 
